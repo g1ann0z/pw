5b9d34a5895e7d09c58be015ec8c39c34c66fdb9
g1ann0z <giannozcydia@gmail.com>
2024-05-04 17:17:31 +0200

    installBulma

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,61 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pw';
+  length = 0;
+  includeSymbols = false;
+  includeNumbers = false;
+  includeLetters = false;
+  password = '';
+  lenght ='';
+
+  onChangeLength(value: string){
+    const parsedValue = parseInt(value);
+
+    if(!isNaN(parsedValue)){
+      this.length = parsedValue;
+    }
+  }
+  
+
+  onChangeIncludeSymbols(){
+    this.includeSymbols = !this.includeSymbols;
+  }
+ 
+  onChangeUseNumbers(){
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseLetters(){
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onButtonClick(){
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuwyz';
+    const symbols = '!@#$%&*()';
+
+    let validChars = '';
+    if(this.includeLetters) {
+      validChars += letters;
+    }
+
+    if(this.includeNumbers){
+      validChars += numbers;
+    }
+
+    if(this.includeSymbols){
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for(let i = 0; i < this.length; i++){
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+
+
+  }
+
 }
